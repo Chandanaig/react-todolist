17251ecb356ef5f7d72f990c4d74891eceff9762
unknown <c.i.gouda528@gmail.com>
2024-09-21 13:28:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import Header from './components/Header';
+
+import './App.css';
 
 function App() {
-  const headerStyle = {
-    padding: "20px",
-    backgroundColor: "#282c34",
-    color: "white",
-    textAlign: "center",
-  };
-
-  const bodyStyle = {
-    padding: "500px",
-    textAlign: "center",
-  };
-
-  const footerStyle = {
-    padding: "20px",
-    backgroundColor: "#282c34",
-    color: "white",
-    textAlign: "center",
-  };
-
   return (
-    <div>
-      <header style={headerStyle}>
-        <h1>header</h1>
-      </header>
-      <main style={bodyStyle}>
-        <h1>
-          Body
-        </h1>
-      </main>
-      <footer style={footerStyle}>
-        <h1>footer</h1>
-      </footer>
+    <div className="App">
+      <Header />
+     
+     
     </div>
   );
 }
